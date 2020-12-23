0176c5e71946a0d2c927168c699fa06ca19340f3
plmwa <townstgak@gmail.com>
2020-12-23 17:41:50 +0900

    2画面にした

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+//Compornents
+import HomeScreen from './components/HomeScreen';
+import ChartScreen from './components/ChartScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Chart: ChartScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
---
+++ components/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import styles from '../assets/style/styles.js';
+
+class HomeScreen extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello World!</Text>
+        <Button
+          title="Go to Chart"
+          onPress={() => this.props.navigation.navigate('Chart')}
+        />
+        
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
