06d7d24cad706499aa4c5d6b640c2c049e21e4ff
Chaha1n <Chahaaaaan@gmail.com>
2020-12-23 21:26:44 +0900

    add time input field

--- assets/style/styles.js
+++ assets/style/styles.js
@@ -1,12 +1,21 @@
 import {StyleSheet} from 'react-native';
+
 export default StyleSheet.create({
-    
+
     container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    
-    
+    modal: {
+
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 50,
+        padding: 50,
+        backgroundColor: 'white'
+    }
+
+
 });
--- components/parts/AddSleepTimeModal.js
+++ components/parts/AddSleepTimeModal.js
@@ -1,33 +1,42 @@
+import React from 'react';
 import {Button, Text, View} from 'react-native';
 import Modal from 'modal-react-native-web';
+import styles from "../../assets/style/styles";
 
-export default class AddSleepTimeModal extends React.Component{
+export default class AddSleepTimeModal extends React.Component {
 
-    state={
-        isModalVisible:false,
+    state = {
+        isModalVisible: false,
     }
 
     constructor() {
         super();
-        this.state.isModalVisible=false
+        this.state.isModalVisible = false
         this.toggleModal = this.toggleModal.bind(this);
     }
 
     toggleModal() {
-        this.setState({isModalVisible:!this.state.isModalVisible})
+        this.setState({isModalVisible: !this.state.isModalVisible})
     };
 
     render() {
         return (
-            <View style={{flex: 1}}>
+            <View style={styles.container}>
                 <Button title="Show modal" onPress={this.toggleModal}/>
-
                 <Modal
-                    visible={this.state.isModalVisible}>
-                    <View style={{flex: 1}}>
+                    visible={this.state.isModalVisible}
+                    animationType={"slide"}
+                    style={styles.container}>
+                    <View style={styles.container}>
                         <Text>Hello!</Text>
-                        <textarea></textarea>
-                        <Button title="Hide modal" onPress={this.toggleModal}/>
+                        <input type="time" id="go_to_bed" required></input>
+                        <input type="time" id="get_up" required></input>
+                        <Button title="記録" onPress={()=>{
+                            let goTo = document.getElementById("go_to_bed").value;
+                            let getUp = document.getElementById("get_up").value;
+                            console.log(goTo,getUp);//TODO save times
+                            this.toggleModal();
+                        }}/>
                     </View>
                 </Modal>
             </View>
