ddf3b5555d4f38960013f47a1df03639a7b51baa
Chaha1n <Chahaaaaan@gmail.com>
2020-12-26 12:41:11 +0900

    add date input form

--- components/parts/SleepTimeModal.js
+++ components/parts/SleepTimeModal.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Button, Text, View} from 'react-native';
 import Modal from 'modal-react-native-web';
 import styles from "../../assets/style/styles";
+import Storage from 'react-native-storage';
+import AsyncStorage from '@react-native-community/async-storage';//
 
 export default class SleepTimeModal extends React.Component {
 
@@ -20,20 +22,36 @@ export default class SleepTimeModal extends React.Component {
     };
 
     render() {
+        const defaultDate = ()=>{
+            const d = new Date();
+            const year   = d.getFullYear();
+            const month  = ("0"+(d.getMonth()+1)).slice(-2);
+            const day    = ("0"+d.getDate()).slice(-2);
+            return `${year}-${month}-${day}`;
+        }
         return (
             <View style={styles.container}>
                 <Button title="Show modal" onPress={this.toggleModal}/>
                 <Modal
                     visible={this.state.isModalVisible}
                     animationType={"slide"}
-                    style={styles.container}>
-                    <View style={styles.container}>
+                    style={styles.container}
+                    transparent={true}
+                >
+                    <View style={styles.modal}>
                         <Text>Hello!</Text>
+                        <input type="date" value={defaultDate()}></input>
                         <input type="time" id="go_to_bed" required></input>
                         <input type="time" id="get_up" required></input>
                         <Button title="記録" onPress={()=>{
+
                             let goTo = document.getElementById("go_to_bed").value;
                             let getUp = document.getElementById("get_up").value;
+
+                            //init react-native-storage
+                            const storage = new Storage({
+                                storageBackend:AsyncStorage,
+                            });
                             console.log(goTo,getUp);//TODO save times
                             this.toggleModal();
                         }}/>
