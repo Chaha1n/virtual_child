95a5b74b6194712b537993a7e821246b7c3c5be5
Chaha1n <Chahaaaaan@gmail.com>
2020-12-26 13:15:12 +0900

    changed to using state on form

--- components/parts/SleepTimeModal.js
+++ components/parts/SleepTimeModal.js
@@ -3,31 +3,72 @@ import {Button, Text, View} from 'react-native';
 import Modal from 'modal-react-native-web';
 import styles from "../../assets/style/styles";
 import Storage from 'react-native-storage';
-import AsyncStorage from '@react-native-community/async-storage';//
+import AsyncStorage from '@react-native-community/async-storage';
+import {TextInput} from "react-native-web";
+//
 
 export default class SleepTimeModal extends React.Component {
 
     state = {
-        isModalVisible: false,
+        isModalVisible : false,
+        dateField      : this._getDefaultDate().toString(),
+        goToBedField   : "00:00",
+        getUpField     : "00:00",
     }
 
     constructor() {
         super();
         this.state.isModalVisible = false
-        this.toggleModal = this.toggleModal.bind(this);
+        this.toggleModal          = this.toggleModal.bind(this);
     }
 
     toggleModal() {
         this.setState({isModalVisible: !this.state.isModalVisible})
     };
 
+    _getDefaultDate(){
+        const d      = new Date();
+        const year   = d.getFullYear();
+        const month  = ("0"+(d.getMonth()+1)).slice(-2);
+        const day    = ("0"+d.getDate()).slice(-2);
+
+        return `${year}-${month}-${day}`;
+    }
+    _handleDateChange = inputDate =>{
+        this.setState({dateField:inputDate});
+        console.log(inputDate);
+    }
+    _handleGetUpChange = inputGetUp =>{
+        this.setState({getUpField:inputGetUp});
+    }
+    _handleGoToChange = inputGoTo =>{
+        this.setState({goToBedField:inputGoTo});
+    }
     render() {
-        const defaultDate = ()=>{
-            const d = new Date();
-            const year   = d.getFullYear();
-            const month  = ("0"+(d.getMonth()+1)).slice(-2);
-            const day    = ("0"+d.getDate()).slice(-2);
-            return `${year}-${month}-${day}`;
+        const recordTimeOfSleeping = ()=>{
+            let date  = this.state.dateField;
+            let goTo  = this.state.goToBedField;
+            let getUp = this.state.getUpField;
+
+            let sleepTime = goTo - getUp > 0 ? goTo - getUp : 0;
+
+            //init react-native-storage
+            const storage = new Storage({
+                storageBackend:AsyncStorage,
+            });
+            console.log(date,goTo,getUp);
+            storage.save({
+                key:date,
+                data:{
+                    sleepTime:sleepTime,
+                }
+            }).then(()=>{
+                this.toggleModal();
+            }).catch((e)=>{
+                console.error(e);
+            })
+
+
         }
         return (
             <View style={styles.container}>
@@ -40,21 +81,10 @@ export default class SleepTimeModal extends React.Component {
                 >
                     <View style={styles.modal}>
                         <Text>Hello!</Text>
-                        <input type="date" value={defaultDate()}></input>
-                        <input type="time" id="go_to_bed" required></input>
-                        <input type="time" id="get_up" required></input>
-                        <Button title="記録" onPress={()=>{
-
-                            let goTo = document.getElementById("go_to_bed").value;
-                            let getUp = document.getElementById("get_up").value;
-
-                            //init react-native-storage
-                            const storage = new Storage({
-                                storageBackend:AsyncStorage,
-                            });
-                            console.log(goTo,getUp);//TODO save times
-                            this.toggleModal();
-                        }}/>
+                        <input type="date" value = {this.state.dateField} onChange = {this._handleDateChange}></input>
+                        <input type="time" value = {this.state.getUpField} onChangeText = {this._handleGetUpChange}></input>
+                        <input type="time" value = {this.state.goToBedField} onChangeText = {this._handleGoToChange}></input>
+                        <Button title="記録" onPress={recordTimeOfSleeping}/>
                     </View>
                 </Modal>
             </View>
